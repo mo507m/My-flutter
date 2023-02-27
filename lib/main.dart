ef4e3454894164426e4b1c8ac7c3bf811d8bc181
mo507m <mo155mo@hotmail.com>
2023-02-27 20:42:37 +0300

    friest commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home:Scaffold(
+      appBar: AppBar(title: Text("my Flutter")),
+      body:Center(
+        child:Column(
+       mainAxisAlignment:MainAxisAlignment.center,
+       crossAxisAlignment:CrossAxisAlignment.center,
+       // ignore: sort_child_properties_last
+       children: [
+        Container(color: Colors.red, child: const Text("Hi")),
+       Container(color: Colors.blue, child: const Text("flutter")),
+       ]
+    )),
+  backgroundColor: Colors.green,
+    ),
+  ));
+}
+
